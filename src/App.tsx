1ca1c5336f058220329d647d6fcee6c863daff3e
Devansh Beserwal <devansh.beserwal@gmail.com>
2023-09-10 23:59:55 +0530

    navbar and bg added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import bg1 from "./images/bg1.png";
 import './App.css';
 
 function App() {
   return (
-   <div>hello didididd
+   <div>
+      <body>
+    <header>
+        <nav className="navbar">
+            <div className="logo">
+                <a href="/">Your Logo</a>
+            </div>
+            <ul className="nav-links">
+                <li><a href="/">Home</a></li>
+                <li><a href="/products">Products</a></li>
+                <li><a href="/cart">Cart</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+<img src= {bg1} alt="Image Description"></img>
 
    </div>
   );
